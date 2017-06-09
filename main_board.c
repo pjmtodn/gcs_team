954991e8c9be5b506a4115ff6feb46131195f109
김영찬 <withgod072@gmail.com>
2017-06-09 15:42:55 +0900

    fixed #3 리뷰 에 대한 수정 사항

--- main_board.c
+++ main_board.c
@@ -5,10 +5,13 @@ int main(int argc, char* arg[])
 	int a,b,c;
 	char operator;
 
-	printf("please enter a expression");
-
 
 	printf("please enter a expression2");
+
+	printf("please enter a expression3");
+
+	printf("please enter a expression4");
+
 
 
 }
